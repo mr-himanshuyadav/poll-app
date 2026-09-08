979f4c1953a8620f9a30a9bd7b668e68bb0c2f50
Himanshu Yadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2026-09-08 16:35:15 +0530

    feat(studio): link overview to participant response details

--- components/live-studio/live-question-workspace.tsx
+++ components/live-studio/live-question-workspace.tsx
@@ -296,6 +296,8 @@ response.id
                     <ResponseWorkspace
                         responseCount={responseCount}
                         participantCount={totalParticipants}
+                        participantsLabel="View all participants & response history"
+                        onParticipantsNavigate={() => {}}
                         overview={
                             <div className="grid gap-6 p-5 lg:grid-cols-[minmax(0,1fr)_280px]">
                                 <ResponseProgressPanel
